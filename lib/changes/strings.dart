76f133709f793fb248477eb646a79c3824dbaaa0
Ajayi Olumide <olumightyajayi@gmail.com>
2024-02-24 22:37:14 +0100

    Updated service section

--- lib/changes/strings.dart
+++ lib/changes/strings.dart
@@ -16,13 +16,13 @@ String miniDescription =
     "Experienced Cross-platform Flutter developer specializing in web and mobile application development with years of proven success in delivering commercially viable applications.  ensuring optimal user satisfaction. Additionally, adept Manual Software Tester skilled in thorough testing methodologies and defect identification.";
 
 String servicesSubHeading =
-    "Since the beginning of my journey as a freelance designer and developer, I've worked in startups and collaborated with talented people to create digital products for both business and consumer use. I offer a wide range of services, including brand design, programming and teaching.";
+    "Crafting Interactive Experiences using Flutter Developer & Tech Enthusiast \n Building User-Centric Solutions: Software Engineer & Frontend Expert Solving Problems, \n Building Solutions: My Software Engineering Skills. \n From Design to Code: Frontend Developer & Flutter Specialist";
 //
 
 String protfolioSubHeading =
     "Since the beginning of my journey as a designer and developer, I have created digital products for business and consumer use. This is a little bit.";
 const String aboutMeHeadline =
-    'I\'m Sudesh Bandara, a Mobile App developer, Web developer and UI designer.';
+    'I\'m Ajayi Olumide, a Mobile App developer, Web developer and Manual Software Tester.';
 
 const String aboutMeDetail =
     'I\'m a Fresh Graduate as Software Engineer from Quanrt University, UK. I have been developing mobile apps for over 4 years now. I have worked in teams for various startups and helped them in launching their prototypes and got valuable learning experience. Been a CEO/Co-Founder of Onyxsio, Sri Lanka. Currently working as an Associate Software Engineer at Onyxsio.';
